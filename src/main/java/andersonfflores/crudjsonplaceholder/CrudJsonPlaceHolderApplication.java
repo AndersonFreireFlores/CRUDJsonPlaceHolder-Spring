33c4d2ba70200c335583e7d5bf85f5f20e0dffa8
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-06 09:56:02 -0300

    first commit

---
+++ src/main/java/andersonfflores/crudjsonplaceholder/CrudJsonPlaceHolderApplication.java
@@ -0,0 +1,13 @@
+package andersonfflores.crudjsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJsonPlaceHolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJsonPlaceHolderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andersonfflores/crudjsonplaceholder/CrudJsonPlaceHolderApplicationTests.java
@@ -0,0 +1,13 @@
+package andersonfflores.crudjsonplaceholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJsonPlaceHolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
